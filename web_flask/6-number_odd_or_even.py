1af4b7d1ecc032f5b02199aef8250530fca49045
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-04-22 21:58:55 -0500

    [ADD] AirBnB clone - Web framework

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def route_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return 'C {}'.format(text).replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>/', strict_slashes=False)
+def pytho_is_cool(text):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def int_number(n):
+    return '{} is a number'.format(n).replace('_', ' ')
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    letter = "even" if n % 2 == 0 else "odd"
+    return render_template('6-number_odd_or_even.html', n=n, letter=letter)
+
+
+if __name__ == '__main__':
+    app.run()
